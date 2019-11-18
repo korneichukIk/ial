d13d81c019f49dec526018dca27b65010b937de5
Ivan <ikorneic@redhat.com>
2019-11-18 16:16:07 +0100

    Upload second project

---
+++ ial_du2/c016/c016.c
@@ -0,0 +1,198 @@
+/* c016.c: **********************************************************}
+{* Téma:  Tabulka s Rozptýlenými Položkami
+**                      První implementace: Petr Přikryl, prosinec 1994
+**                      Do jazyka C prepsal a upravil: Vaclav Topinka, 2005
+**                      Úpravy: Karel Masařík, říjen 2014
+**                              Radek Hranický, 2014-2018
+**
+** Vytvořete abstraktní datový typ
+** TRP (Tabulka s Rozptýlenými Položkami = Hash table)
+** s explicitně řetězenými synonymy. Tabulka je implementována polem
+** lineárních seznamů synonym.
+**
+** Implementujte následující procedury a funkce.
+**
+**  HTInit ....... inicializuje tabulku před prvním použitím
+**  HTInsert ..... vložení prvku
+**  HTSearch ..... zjištění přítomnosti prvku v tabulce
+**  HTDelete ..... zrušení prvku
+**  HTRead ....... přečtení hodnoty prvku
+**  HTClearAll ... zrušení obsahu celé tabulky (inicializace tabulky
+**                 poté, co již byla použita)
+**
+** Definici typů naleznete v souboru c016.h.
+**
+** Tabulka je reprezentována datovou strukturou typu tHTable,
+** která se skládá z ukazatelů na položky, jež obsahují složky
+** klíče 'key', obsahu 'data' (pro jednoduchost typu float), a
+** ukazatele na další synonymum 'ptrnext'. Při implementaci funkcí
+** uvažujte maximální rozměr pole HTSIZE.
+**
+** U všech procedur využívejte rozptylovou funkci hashCode.  Povšimněte si
+** způsobu předávání parametrů a zamyslete se nad tím, zda je možné parametry
+** předávat jiným způsobem (hodnotou/odkazem) a v případě, že jsou obě
+** možnosti funkčně přípustné, jaké jsou výhody či nevýhody toho či onoho
+** způsobu.
+**
+** V příkladech jsou použity položky, kde klíčem je řetězec, ke kterému
+** je přidán obsah - reálné číslo.
+*/
+
+#include "c016.h"
+
+int HTSIZE = MAX_HTSIZE;
+int solved;
+
+/*          -------
+** Rozptylovací funkce - jejím úkolem je zpracovat zadaný klíč a přidělit
+** mu index v rozmezí 0..HTSize-1.  V ideálním případě by mělo dojít
+** k rovnoměrnému rozptýlení těchto klíčů po celé tabulce.  V rámci
+** pokusů se můžete zamyslet nad kvalitou této funkce.  (Funkce nebyla
+** volena s ohledem na maximální kvalitu výsledku). }
+*/
+
+int hashCode ( tKey key ) {
+	int retval = 1;
+	int keylen = strlen(key);
+	for ( int i=0; i<keylen; i++ )
+		retval += key[i];
+	return ( retval % HTSIZE );
+}
+
+/*
+** Inicializace tabulky s explicitně zřetězenými synonymy.  Tato procedura
+** se volá pouze před prvním použitím tabulky.
+*/
+
+void htInit ( tHTable* ptrht ) {
+	for (int i = 0; i < HTSIZE; i++) {
+		(*ptrht)[i] = NULL;
+	}
+}
+
+/* TRP s explicitně zřetězenými synonymy.
+** Vyhledání prvku v TRP ptrht podle zadaného klíče key.  Pokud je
+** daný prvek nalezen, vrací se ukazatel na daný prvek. Pokud prvek nalezen není,
+** vrací se hodnota NULL.
+**
+*/
+
+tHTItem* htSearch ( tHTable* ptrht, tKey key ) {
+	tHTItem *to_search = (*ptrht)[hashCode(key)];
+	while (to_search) {
+		if (key == to_search->key) {
+			return to_search;
+		}
+
+		to_search = to_search->ptrnext;
+	}
+
+	return NULL;
+}
+
+/*
+** TRP s explicitně zřetězenými synonymy.
+** Tato procedura vkládá do tabulky ptrht položku s klíčem key a s daty
+** data.  Protože jde o vyhledávací tabulku, nemůže být prvek se stejným
+** klíčem uložen v tabulce více než jedenkrát.  Pokud se vkládá prvek,
+** jehož klíč se již v tabulce nachází, aktualizujte jeho datovou část.
+**
+** Využijte dříve vytvořenou funkci htSearch.  Při vkládání nového
+** prvku do seznamu synonym použijte co nejefektivnější způsob,
+** tedy proveďte.vložení prvku na začátek seznamu.
+**/
+
+void htInsert ( tHTable* ptrht, tKey key, tData data ) {
+	tHTItem *to_search = htSearch(ptrht, key);
+
+	if (to_search == NULL) {
+		tHTItem *to_insert = malloc(sizeof(struct tHTItem));
+
+		if (to_insert == NULL) {
+			return;
+		}
+
+		to_insert->key = key;
+		to_insert->data = data;
+		int hkey = hashCode(key);
+		to_insert->ptrnext = (*ptrht)[hkey];
+
+		(*ptrht)[hkey] = to_insert;
+	}else{
+		to_search->data = data;
+	}
+}
+
+/*
+** TRP s explicitně zřetězenými synonymy.
+** Tato funkce zjišťuje hodnotu datové části položky zadané klíčem.
+** Pokud je položka nalezena, vrací funkce ukazatel na položku
+** Pokud položka nalezena nebyla, vrací se funkční hodnota NULL
+**
+** Využijte dříve vytvořenou funkci HTSearch.
+*/
+
+tData* htRead ( tHTable* ptrht, tKey key ) {
+	tHTItem *to_search = htSearch(ptrht, key);
+
+	if (to_search != NULL) {
+		return &(to_search->data);
+	}
+
+	return NULL;
+}
+
+/*
+** TRP s explicitně zřetězenými synonymy.
+** Tato procedura vyjme položku s klíčem key z tabulky
+** ptrht.  Uvolněnou položku korektně zrušte.  Pokud položka s uvedeným
+** klíčem neexistuje, dělejte, jako kdyby se nic nestalo (tj. nedělejte
+** nic).
+**
+** V tomto případě NEVYUŽÍVEJTE dříve vytvořenou funkci HTSearch.
+*/
+
+void htDelete ( tHTable* ptrht, tKey key ) {
+	tHTItem *to_delete = (*ptrht)[hashCode(key)];
+
+	if (to_delete == NULL) {
+		return;
+	}
+
+	tHTItem *prev_item = NULL, *next_item = NULL;
+	while (to_delete != NULL) {
+		next_item = to_delete->ptrnext;
+
+		if (to_delete->key == key) {
+			free(to_delete);
+
+			if (prev_item == NULL) {
+				(*ptrht)[hashCode(key)] = next_item;
+				return;
+			}
+
+			prev_item->ptrnext = next_item;
+			return;
+		}
+	}
+
+}
+
+/* TRP s explicitně zřetězenými synonymy.
+** Tato procedura zruší všechny položky tabulky, korektně uvolní prostor,
+** který tyto položky zabíraly, a uvede tabulku do počátečního stavu.
+*/
+
+void htClearAll ( tHTable* ptrht ) {
+	for (int i = 0; i < HTSIZE; i++) {
+		tHTItem *temp_item = (*ptrht)[i];
+
+		while (temp_item != NULL) {
+			tHTItem *to_delete = temp_item;
+			temp_item = temp_item->ptrnext;
+			free(to_delete);
+		}
+
+		(*ptrht)[i] = NULL;
+	}
+}
